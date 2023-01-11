c2e5925d34c5895632b9b875f5d56cdd719c3a15
George0717 <raden7george@gmail.com>
2023-01-11 19:16:57 +0700

    Update foodRoute.js

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -1,4 +1,4 @@
-import  express from "express";
+const express = require('express')
 import { getUsers, getFoodById, saveUser, UpdateFood, DeleteFood } from "../controller/foodController.js";
 
 const router = express.Router()
